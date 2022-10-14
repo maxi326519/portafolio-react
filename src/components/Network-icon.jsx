1b991f974443ca97b4007af370d97f7adfedddc4
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-14 17:23:36 -0300

    Update

--- src/components/Network-icon.jsx
+++ src/components/Network-icon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NetworkIcon({icon}){
     return(
         <div className='network-icon'>
-            <img className='svg' src={icon}></img>
+            <img className='svg' src={icon} ></img>
         </div>
     )
 }
