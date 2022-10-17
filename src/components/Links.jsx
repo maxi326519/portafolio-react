636315d83cc81fdf06b409086657330535e001f0
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-17 19:31:26 -0300

    Add target _blank

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -8,8 +8,8 @@ import linkedin from '../assets/svg/network/linkedin.svg'
 export default function Links(){
     return (
         <div className='links-container'>
-            <a href='https://github.com/maxi326519'><img src={github} alt='arrow'/></a>
-            <a href='https://www.linkedin.com/in/maxi-garcia-fsd/'><img src={linkedin} alt='arrow'/></a>
+            <a target='_blank' rel='noreferrer' href='https://github.com/maxi326519'><img src={github} alt='arrow'/></a>
+            <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/maxi-garcia-fsd/'><img src={linkedin} alt='arrow'/></a>
         </div>
     )
 }
