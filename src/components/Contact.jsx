19622ee0a8a0c22ec6bdc75c7b82f815fcde7587
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-18 00:38:46 -0300

    First commit - Add Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
-export default function Contact (){
-    return(
-        <div className='container-form' id='contact'>
-            <form>
-                Aca esta el formulario de contacto  
-            </form>
+import "./styles/Contact.scss";
+
+export default function Contact() {
+  return (
+    <div className="container-form" id="contact">
+      <h2 className="section-title">Contact</h2>
+      <div className="width-container">
+        <div className="form-info">
+          <span>email</span>
         </div>
-    )
+        <form>
+          <div className="form-input">
+            <label>Name</label>
+            <input type="text" id="name"></input>
+          </div>
+          <div className="form-input">
+            <label>E-mail</label>
+            <input type="email" id="email"></input>
+          </div>
+          <div className="form-input">
+            <label>Subject</label>
+            <input type="email" id="subject"></input>
+          </div>
+          <div className="form-input">
+            <label>Message</label>
+            <textarea type="text" id="textarea"></textarea>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
