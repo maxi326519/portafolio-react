43a38a16ff55f679b49bd254620f791d51aaa4ba
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-11-16 21:40:52 -0300

    Update styles - Add footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home'
 import Proyects from './components/Proyects'
 import Skills from './components/Skills'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Proyects/>
       <Skills/>
 {/*       <Contact/> */}
+{/*     <Footer/> */}
     </div>
   );
 }
