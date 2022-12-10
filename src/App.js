b867c623bbee4493bdcd85e7745cd4e1281158cf
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-12-10 02:31:57 -0300

    Update About

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 
 // Componentes
-import Nav from './components/Nav'
-import Home from './components/Home'
-import Proyects from './components/Proyects'
-import Skills from './components/Skills'
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import Proyects from './components/Proyects';
+import Skills from './components/Skills';
 /* import Contact from './components/Contact' */
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Home/>
+      <About/>
       <Proyects/>
       <Skills/>
 {/*       <Contact/> */}
---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+export default function About(){
+    return(
+        <div>
+            <div>
+                <div>
+                    <img/>
+                </div>
+                <div>
+                    <h1>About</h1>
+                    <h3>Hi, I'm Maximiliano</h3>
+                    <p>i'm web developer from Argentina. My preferred tools are React in FrontEnd width others needs tools,
+                        and NodeJS for the backend width then needs tools too, but i'm know more importants tecnologies too. I'm responsable, </p>
+                </div>
+            </div>
+        </div>
+    )
+}
